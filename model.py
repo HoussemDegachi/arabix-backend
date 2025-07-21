047847fb8340bc6fcef878b1455d8c7db3622b36
HoussemDegachi <houssemdegach@gmail.com>
2025-07-21 17:08:30 +0200

    auth

--- model.py
+++ model.py
@@ -1,10 +1,13 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
+import os
+from dotenv import load_dotenv
 
 class Transliterator:
     def __init__(self, model_name_or_path: str):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
+        load_dotenv()
+        HF_ARABIX_BACKEND=os.getenv("HF_ARABIX_BACKEND")
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, token=HF_ARABIX_BACKEND)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path, token=HF_ARABIX_BACKEND)
 
     def transliterate(self, text: str) -> str:
         inputs = self.tokenizer(text, return_tensors="pt")
