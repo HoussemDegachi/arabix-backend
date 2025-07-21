f370c4bc7f583266d39c8b71fa2337069ae36d28
HoussemDegachi <houssemdegach@gmail.com>
2025-07-21 14:03:58 +0200

    Ai & FastAPI

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from model import Transliterator
+
+app = FastAPI()
+transliterator = Transliterator("../../Model/arabix-small")
+
+class TransliterateRequest(BaseModel):
+    text: str
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+        return JSONResponse(
+        status_code=exc.status_code,
+        content=jsonable_encoder({"detail": exc.detail}),
+    )
+
+@app.get("/transliterate")
+async def transliterate(body: TransliterateRequest, status_code=status.HTTP_200_OK):
+    print(f"New request: {body.text}")
+    try:
+        transliterated_text = transliterator.transliterate(body.text)
+    except:
+         raise HTTPException(status_code=500, detail="An error occured")       
+    print(f"Response: {transliterated_text}")  
+    return {"text": transliterated_text, "message": "Text transliterated successfully"}
---
+++ model.py
@@ -0,0 +1,17 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from huggingface_hub import login
+import os
+
+
+class Transliterator:
+    def __init__(self, model_name_or_path: str):
+        # hf_token = os.getenv("HF_ARABIX_SMALL_KEY")
+        # print(hf_token)
+        # login(token=hf_token)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
+
+    def transliterate(self, text: str) -> str:
+        inputs = self.tokenizer(text, return_tensors="pt")
+        outputs = self.model.generate(**inputs)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
