5222b27c6a2403835083c508846abf5de14b97b9
HoussemDegachi <houssemdegach@gmail.com>
2025-07-22 10:33:36 +0200

    Fixed authentication

--- model.py
+++ model.py
@@ -1,14 +1,27 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from huggingface_hub import login
 import os
 from dotenv import load_dotenv
 
+
 class Transliterator:
     def __init__(self, model_name_or_path: str):
+        self.model_path = model_name_or_path
+        self.login()
+        self.load_tokenizer()
+        self.load_model()
+
+    def login(self):
         load_dotenv()
-        HF_ARABIX_BACKEND=os.getenv("HF_ARABIX_BACKEND")
+        HF_ARABIX_BACKEND = os.getenv("HF_ARABIX_BACKEND")
         print(HF_ARABIX_BACKEND)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, token=HF_ARABIX_BACKEND)
-        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path, token=HF_ARABIX_BACKEND)
+        login(token=HF_ARABIX_BACKEND)
+
+    def load_tokenizer(self):
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_path)
+
+    def load_model(self):
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.model_path)
 
     def transliterate(self, text: str) -> str:
         inputs = self.tokenizer(text, return_tensors="pt")
