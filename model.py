b03fe7e66068adc2020e4fb369c78529f1a6e1ad
HoussemDegachi <houssemdegach@gmail.com>
2025-07-22 10:15:12 +0200

    Fixed cache problem

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ class Transliterator:
     def __init__(self, model_name_or_path: str):
         load_dotenv()
         HF_ARABIX_BACKEND=os.getenv("HF_ARABIX_BACKEND")
+        print(HF_ARABIX_BACKEND)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, token=HF_ARABIX_BACKEND)
         self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path, token=HF_ARABIX_BACKEND)
 
